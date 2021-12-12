7d567e6cf1026e6f0f5b3f32a0e629587addfba2
Edgar-Meza <edgar.m.dev@gmail.com>
2021-12-12 13:09:33 -0600

    se cambio funcion de inicio y la forma en la que se muestran los datos

--- js/main.js
+++ js/main.js
@@ -3,34 +3,43 @@ function getAll(){
     .then(res=>res.json())
     .then(data => {
         var container = document.getElementById('container');
-        for (var item in data) {
-            console.log(data[item]);
-            var classelement = data[item].title;
+        container.innerHTML = '<section class="card-user"><div class="head-user-card"><img src="images/image-jeremy.png" alt="people"><div class="data-user"> <span>Report for</span><h1>Jeremy Robson</h1></div></div><div class="range"><span class="active" onclick="getByTime(\'daily\')" id="daily">Daily</span><span onclick="getByTime(\'weekly\')" id="weekly">Weekly</span><span onclick="getByTime(\'monthly\')" id="monthly">Monthly</span></div></section>';
+        for (var n_element in data) {
+            var classelement = data[n_element].title;
             var class_element = classelement.toLowerCase().replace(" ","-");
-            var current = data[item].timeframes.daily.current;
-            var previous = data[item].timeframes.daily.previous;
-            var element = '<section class="card-stat '+class_element+'"><div class="info-card"><div class="title-card"><h2>'+data[item].title+'</h2><img src="images/icon-ellipsis.svg" alt="puntos"></div><span class="actual">'+current+'hrs</span><span class="pasado">Last Week - '+previous+'hrs</span></div></section>';
-            container.innerHTML = container.innerHTML+element;
+            var item_title = data[n_element].title;
+            container.innerHTML = container.innerHTML+'<section class="card-stat '+class_element+'"><div class="info-card"><div class="title-card"><h2 id="title-'+item_title+'"></h2><img src="images/icon-ellipsis.svg" alt="puntos"></div><span class="actual" id="actual-'+item_title+'"></span><span class="pasado" id="pasado-'+item_title+'"></span></div></section>';
+        }
+        for (var item in data) {
+            var tit, act, pas;
+            tit = document.getElementById('title-'+data[item].title);
+            act = document.getElementById('actual-'+data[item].title);
+            pas = document.getElementById('pasado-'+data[item].title);
+            tit.innerHTML = data[item].title;
+            act.innerHTML = data[item].timeframes.daily.current+'hrs';
+            pas.innerHTML = 'Last Week - '+data[item].timeframes.daily.previous+'hrs';
         }
     });
 }
 getAll();
 
 function getByTime(time) {
-    var container = document.getElementById('container');
-    container.innerHTML = '<section class="card-user"><div class="head-user-card"><img src="images/image-jeremy.png" alt="people"><div class="data-user"> <span>Report for</span><h1>Jeremy Robson</h1></div></div><div class="range"><span onclick="getByTime(\'daily\')">Daily</span><span onclick="getByTime(\'weekly\')">Weekly</span><span onclick="getByTime(\'monthly\')">Monthly</span></div></section>';
+    var ranges = ["daily", "weekly", "monthly"];
+    for (var i=0; i<ranges.length; i++) {
+        document.getElementById(ranges[i]).classList.remove('active');
+    }
+    document.getElementById(time).classList.add('active');
     fetch(window.location.href+'data.json')
     .then(res=>res.json())
     .then(data => {
-        var container = document.getElementById('container');
         for (var item in data) {
-            console.log(data[item]);
-            var classelement = data[item].title;
-            var class_element = classelement.toLowerCase().replace(" ","-");
-            var current = data[item].timeframes[time].current;
-            var previous = data[item].timeframes[time].previous;
-            var element = '<section class="card-stat '+class_element+'"><div class="info-card"><div class="title-card"><h2>'+data[item].title+'</h2><img src="images/icon-ellipsis.svg" alt="puntos"></div><span class="actual">'+current+'hrs</span><span class="pasado">Last Week - '+previous+'hrs</span></div></section>';
-            container.innerHTML = container.innerHTML+element;
+            var tit, act, pas;
+            tit = document.getElementById('title-'+data[item].title);
+            act = document.getElementById('actual-'+data[item].title);
+            pas = document.getElementById('pasado-'+data[item].title);
+            tit.innerHTML = data[item].title;
+            act.innerHTML = data[item].timeframes[time].current+'hrs';
+            pas.innerHTML = 'Last Week - '+data[item].timeframes[time].previous+'hrs';
         }
     });
 }
